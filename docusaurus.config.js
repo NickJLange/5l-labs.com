642f3c889f1f3db75c36b397a5a703ac6a85eb62
Nick Lange <nick.lange@gmail.com>
2022-11-06 01:35:04 -0400

    Initial Commit - Pre-Customization Using Default Theme
    Exploring custom components
    
    Checking if adding helps...

---
+++ docusaurus.config.js
@@ -0,0 +1,131 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const {themes} = require('prism-react-renderer');
+const lightTheme = themes.github;
+const darkTheme = themes.dracula;
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: '5L Labs',
+  tagline: 'Commercial Privacy First.',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: '5L-Labs', // Usually your GitHub org/user name.
+  projectName: '5L-labs.com', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+  markdown: {
+    mermaid: true,
+  },
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+  themes: ['@docusaurus/theme-mermaid'],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: '5L-Labs',
+        logo: {
+          alt: '5L Labs Logo',
+          src: 'img/5L-Labs.svg',
+        },
+        items: [
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/orgs/5L-Labs/',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      mermaid: {
+        theme: {
+          light: 'dark',
+          dark: 'forest'
+        },
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog/',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/5L-Labs',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/NickJLange',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightTheme,
+        darkTheme: darkTheme,
+      },
+    }),
+};
+
+module.exports = config;
